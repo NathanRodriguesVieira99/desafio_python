f524d16f8ebafe85cea773f883ba67eebd60b022
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-24 08:47:50 -0300

    feat(challenge01): challange 01 done

--- desafio_01.py
+++ desafio_01.py
@@ -1,8 +1,46 @@
-print('Informe o valor: ')
+class N:
+    typed_numbers = set()
 
-valorInformado = (input('Digite o valor'))
+# função que recebe o(s) número(s) informados como parâmetro
+    def is_happy(self, typed_number: int) -> bool:
+
+        #  se o(s) números for(em) infeliz(es) retorna False
+        if typed_number in N.typed_numbers:
+            N.typed_numbers.clear()
+            return False
+
+        N.typed_numbers.add(typed_number)
+
+        soma = sum(int(digito)**2 for digito in str(typed_number))
+
+# se o(s) números for(em) feliz(es) retorna True
+        if soma == 1:
+            N.typed_numbers.clear()
+            return True
+
+        return self.is_happy(soma)
 
 
-def N():
-    n = 1
-    isUnit = n % 10 
+n = N()
+
+# informa o(s) número(s)
+entrada = input("Digite um  número: ")
+
+
+# pega o(s) número(s) e valida se o cálculo está correto (feliz ou infeliz)
+
+for valor in entrada.split():
+    try:
+        typed_number = int(valor)
+        if n.is_happy(typed_number):
+            print(f"Feliz: {typed_number}")
+        else:
+            print(f"Infeliz: {typed_number}")
+    except ValueError:
+        print(f"Ignorado (não é número válido): {valor}")
+
+"""
+Usamos de referência uma pergunta de um usuário no Stack Overflow
+
+link de referência: https://stackoverflow.com/questions/63751186/happy-number-string-python-function
+"""
